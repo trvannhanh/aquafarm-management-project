7495d14d4d0f2decb1665de6ab13206ca06a7d9a
ntat <nguyentrananhtuyen100000@gmail.com>
2025-07-31 15:12:57 +0700

    remove .vs file, create areaviewmodel, create constraint for areatotal

---
+++ AquaFarmApp/AquaFarmApp/ViewModels/CreateAreaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AquaFarmApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AquaFarmApp.ViewModels
+{
+    public class AreaCreateModel
+    {
+        [Required(ErrorMessage = "Area name is required")]
+        [StringLength(50, ErrorMessage = "Maximum 50 characters")]
+        public string AreaName { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string AreaStatus { get; set; }
+
+        [Required(ErrorMessage = "Size is required")]
+        [Range(1, 10000, ErrorMessage = "Size must be between 1 and 10000")]
+        public double AreaSize { get; set; }
+
+        [Required(ErrorMessage = "Type of water is required")]
+        public string TypeOfWater { get; set; }
+    }
+}
